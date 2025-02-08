2ff54894bd18d79a66f69c54a42c24d5e5167d67
Natália Luiza <natalialuizads@gmail.com>
2025-02-08 17:16:20 -0300

    build: inclui jest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'boilerplate-angular test'
+  title = 'boilerplate-angular'
 }
